177cb3b00022e6cb4271ef606611435f3e9f1ed9
blockvished <jcmvishael@gmail.com>
2025-08-06 13:37:25 +0000

    add user and video models

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 study process of nodejs
 cors whitelisting
+research about bson data as opposed to json
+why index true is good for database for searchability
+mongodb aggregation pipeline
---
+++ src/models/user.model.js
@@ -0,0 +1,80 @@
+import mongoose, { Schema } from 'mongoose';
+import jwt from 'jsonwebtoken';
+import bcrypt from 'bcrypt';
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+      index: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    fullName: {
+      type: String,
+      required: true,
+      trim: true,
+      index: true,
+    },
+    avatar: {
+      type: String,
+      required: true,
+    },
+    coverImage: {
+      type: String, // cloudinary url
+    },
+    watchHistory: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'Video',
+      },
+    ],
+    password: {
+      type: String,
+      required: [true, 'Password is required'],
+    },
+    refreshToken: {
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
+
+// function keyword is important bc arrow fn doesnt have reference of `this` and thus it wont have the values of userschema but is needed
+userSchema.pre('save', async function (next) {
+  if (this.isModified('password')) {
+    this.password = bcrypt.hash(this.password, 7);
+  }
+  next();
+});
+
+userSchema.methods.isPasswordCorrect = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+userSchema.methods.generateAccessToken = async function (password) {
+  return jwt.sign(
+    { _id: this._id, email: this.email },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+      expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
+    }
+  );
+};
+
+userSchema.methods.generateRefreshToken = async function (password) {
+  return jwt.sign({ _id: this._id }, process.env.REFRESH_TOKEN_SECRET, {
+    expiresIn: process.env.REFRESH_TOKEN_EXPIRY,
+  });
+};
+
+export const User = mongoose.model('User', userSchema);
